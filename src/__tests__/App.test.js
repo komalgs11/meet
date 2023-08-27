820d97d3a4bcd628974fba5a97e69f9184639cb4
komalgs11 <komalpatil26.kp@gmail.com>
2023-08-27 13:38:32 +0200

    created test suit and added app test file

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("<App /> component", () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  });
+
+  test("renders list of events", () => {
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
+
+  test("renders CitySearch", () => {
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+  });
+
+  test("renders NumberOfEvents", () => {
+    expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
+  });
+});
