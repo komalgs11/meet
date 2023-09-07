c0000ad18db44b2fdb57854870288e814019bfcc
komalgs11 <komalpatil26.kp@gmail.com>
2023-09-07 14:59:51 +0200

    updated event function for offline use

--- src/api.js
+++ src/api.js
@@ -77,7 +77,6 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
 
@@ -92,7 +91,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
